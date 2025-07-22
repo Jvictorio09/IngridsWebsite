b3933b8af6a345bd550042bd21f93aaa10ed5391
Julia <officialpatchwork07@gmail.com>
2025-07-23 00:55:36 +0800

    updated

--- myApp/urls.py
+++ myApp/urls.py
@@ -1,21 +1,24 @@
 from . import views
 from django.urls import path
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("", views.index, name='index'),
-    path("about/", views.about, name='about'),
-    path("bookings/", views.bookings, name='booking'),
-    path("readings/", views.readings, name='readings'),
-    path("business/", views.business, name='business'),
-    path("guidanceplan/", views.guidanceplan, name='guidanceplan'),
-    path("shop/", views.shop, name='shop'),
-    path("testshop/", views.testshop, name='testshop'),
-    path("contact/", views.contact, name='contact'),
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('bookings/', views.bookings, name='booking'),
+    path('readings/', views.readings, name='readings'),
+    path('business/', views.business, name='business'),
+    path('guidanceplan/', views.guidanceplan, name='guidanceplan'),
+    path('shop/', views.shop, name='shop'),
+    path('testshop/', views.testshop, name='testshop'),
+    path('contact/', views.contact, name='contact'),
 
-    path("send-free-guide/", views.send_free_guide, name="send_free_guide"),
+    path('send-free-guide/', views.send_free_guide, name='send_free_guide'),
      path('product/<int:product_id>/', views.product_detail_view, name='product_detail'),
 
     path('paypal/create-order/<int:product_id>/', views.create_paypal_order, name='create_paypal_order'),
     path('paypal/capture-order/', views.capture_paypal_order, name='capture_paypal_order'),
     
+
+    path('google01b88af1aaba8456.html', TemplateView.as_view(template_name='google01b88af1aaba8456.html', content_type='text/html')),
 ]
